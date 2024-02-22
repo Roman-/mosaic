f7581f8765e483316059caa7d1001f593aa9313c
Roman Strakhov <roman.strakhov@alberblanc.com>
2024-02-22 11:42:31 +0400

    fix texts

--- js/texts.js
+++ js/texts.js
@@ -16,16 +16,16 @@ Txt.littleHintUnder = "Mouse wheel to zoom in/out. Drag the box or the backgroun
 
 Txt.qaTitlesAndTexts = [
     "<h3 class='mt-4'>What is this?</h3>",
-    "You might've seen artists doing pictures from hundreds, or even thousands of Rubik's cubes. The main hurdle of this art is to turn a full-color photo into a 6-color pixelized version of it. This tool automates the entire process for you.",
+    "You might have seen artists creating pictures from hundreds or even thousands of Rubik's cubes. The main challenge of this art is converting a full-color photo into a 6-color pixelized version. This tool automates the entire process for you.",
     "<h3 class='mt-4'>Video tutorial</h3>",
     "<div class='text-center' id='videoTutorialWrap'></div>",
     "<h3 class='mt-4'>Does it work with black stickers?</h3>",
 "Yes. Click the menu button in the top-right corner of the page, go to palette editor and enable black color. You can change colors as you wish.",
     "<h3 class='mt-4'>I prefer to make mosaic pictures myself. Can I use this website just to split it into sections?</h3>",
-    "Yes. If you have your miniature picture ready (e.g. 60x90 pixel image to make 20x30 cubes mosaic), just upload it here like you would do it with normal pictures. The program will skip all adjustments and just get you to the \"Download PDF\" stage.",
+    "Yes. If you have your miniature picture ready (e.g. 60x90 pixel image to make 20x30 cubes mosaic), simply drag-and-drop it here as you would with normal pictures. The program will skip all adjustments and  directly take you to the \"Download PDF\" stage.",
     "<h3 class='mt-4'>Why some images produce much better mosaics than others?</h3>",
-    "The quality of the source picture matters. A low-contrast photo taken in poor lighing conditions won't work. Beautifiers will also make the photo unsuitable as they flatten some face regions in a way that important face-feature information is lost. <strong>Grayscale pictures are fine</strong> though. Definitely experiment with source images to find the ones that work best.",
-    "<h3 class='mt-4'>How does it work?</h3>",
-    "Good question! Check the <a href='https://github.com/Roman-/mosaic' target='_blank'>Github repository</a> for the explanation.",
+    "The quality of the source picture is important. A low-contrast photo taken in poor lighting conditions won't work. Beautifiers will also render the photo unsuitable, as they flatten some facial regions in a way that important facial feature information is lost. However, <b>grayscale pictures are fine</b>. It's definitely worth experimenting with source images to find the ones that work best.",
+    "<h3 class='mt-4'>How does it even work?</h3>",
+    "Great question! Check the <a href='https://github.com/Roman-/mosaic' target='_blank'>Github repository</a> for the explanation.",
     "<h3 class='mt-4'>I still have questions</h3>",
 "Feel free to ask me anything in <a href='https://www.speedsolving.com/threads/bse-mosaic-new-cube-mosaic-building-software.76139/' target='_blank'>this speedsolving thread</a> and share your thoughts. I wish this tool will improve your experience in Mosaic building!<br>"];
